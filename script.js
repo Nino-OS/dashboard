b400713f4702a85d7658ed5b5ce81fcdf112cc57
team-astral-site <robotic.vb21@gmail.com>
2024-01-24 21:47:51 +0100

    v.1.0

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    document.querySelector(".all-load").style.display = "none";
+    document.querySelector(".loading-footer").style.display = "none";
+    document.querySelector(".all").style.display = "block";
+  }, 3200);
+
+  startProgressBarAnimation();
+});
+
+function startProgressBarAnimation() {
+  document.querySelector(".progress-bar").style.width = "100%";
+}
+
 function scrollToElement(elementSelector, instance = 0) {
   const elements = document.querySelectorAll(elementSelector);
   if (elements.length > instance) {
@@ -23,79 +37,5 @@ document.addEventListener("DOMContentLoaded", function () {
   link3.addEventListener("click", (event) => {
     event.preventDefault();
     scrollToElement(".column");
-  });
-});
-
-function sendEmail() {
-  var name = document.getElementById("name").value;
-  var message = document.getElementById("message").value;
-
-  if (!name) {
-    alert("Bitte gib deinen Namen ein (Discord-Name) 👤");
-    return false;
-  }
-
-  if (!message) {
-    alert("Bitte gib eine Nachricht an 📧");
-    return false;
-  }
-
-  var mailtoLink =
-    "mailto:ninoos.mail@gmail.com" +
-    "?subject=Supportanfrage von " +
-    name +
-    "&body=" +
-    message;
-
-  window.location.href = mailtoLink;
-
-  window.location.href = "https://nino-os.github.io/dashboard/";
-
-  return false;
-}
-
-if (localStorage.getItem("darkMode") === "enabled") {
-  enableDarkMode();
-}
-
-function toggleTheme() {
-  if (localStorage.getItem("darkMode") === "enabled") {
-    disableDarkMode();
-  } else {
-    enableDarkMode();
-  }
-}
-
-function enableDarkMode() {
-  document.body.classList.add("dark-theme");
-  document.querySelector(".theme-toggle").classList.add("active");
-  localStorage.setItem("darkMode", "enabled");
-}
-
-function disableDarkMode() {
-  document.body.classList.remove("dark-theme");
-  document.querySelector(".theme-toggle").classList.remove("active");
-  localStorage.setItem("darkMode", "disabled");
-}
-
-function scrollToElement(elementSelector) {
-  const element = document.querySelector(elementSelector);
-  if (element) {
-    element.scrollIntoView({ behavior: "smooth" });
-  }
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  const link1 = document.getElementById("FAQ");
-  const link2 = document.getElementById("Kontakt");
-
-  link1.addEventListener("click", (event) => {
-    event.preventDefault();
-    scrollToElement(".section1");
-  });
-
-  link2.addEventListener("click", (event) => {
-    event.preventDefault();
-    scrollToElement(".section2");
   });
 });
